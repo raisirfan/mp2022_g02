c4a599686fbfefca7258ead7bff5a618b3d25480
Akmlasyrff <akmal3540@gmail.com>
2022-04-19 03:18:52 +0800

    Add payment, notification & reservation form

---
+++ app/src/main/java/com/example/dqueuebookingappsnew/ReservationForm.java
@@ -0,0 +1,14 @@
+package com.example.dqueuebookingappsnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReservationForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_form);
+    }
+}
